b8fab95e7a3b62716ca7e41bd3be7c74f10178ea
Atul <atulanand94@gmail.com>
2016-11-05 23:58:05 +0530

    disabled authenticated policy for now

--- config/policies.js
+++ config/policies.js
@@ -21,7 +21,7 @@ module.exports.policies = {
   '*': true,
 
   'AuthController': {
-    '*': 'isAuthenticated'
+    '*': true
   },
 
   /***************************************************************************
