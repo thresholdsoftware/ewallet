61116b7f8e0d9307fcbae6b475c99aba151f1444
Atul <atulanand94@gmail.com>
2016-11-05 19:26:38 +0530

    cleanup up

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,22 +1,11 @@
 const signup = (req, res) => {
-
-	
-	let requestBody  ={
-		name: req.body.name,
-		phone : req.body.phone,
-	 	email : req.body.email,
-	 	password:req.body.password	
-	 }
-	
-	User.create(requestBody).then((u)=>{
-			res.status(200).json(u);
-	},(err)=>{
-		res.status(500).json(err);
-	});
+  User.create(req.body).then((u) => {
+    res.status(200).json(u);
+  }, (err) => {
+    res.status(500).json(err);
+  });
 };
 
-
-module.exports ={ 
-
-	signup
+module.exports = {
+  signup
 };
