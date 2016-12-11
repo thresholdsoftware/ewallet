a19695b348d28ad4ac3928eca4b7416932999978
Atul <atulanand94@gmail.com>
2016-12-11 12:57:12 +0530

    rebuild ui

--- client/src/controllers/mainController.js
+++ client/src/controllers/mainController.js
@@ -2,9 +2,13 @@ function mainController($scope, $http, api) {
   const scope = $scope;
   scope.message = 'This is a test screen';
   scope.user = {};
+  scope.bankaccounts = [];
   api.get('/user').then((d) => {
     scope.user = d.data;
   }).catch((err) => console.log(err));
+  api.get('/bank').then((d) => {
+    scope.bankaccounts = d.data;
+  });
 }
 
 module.exports = ['$scope', '$http', 'api', mainController];
