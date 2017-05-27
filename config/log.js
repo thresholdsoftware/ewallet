7127f874bd611829aa61e59aa3fcede5336f48aa
chirag <chirag.b.93@gmail.com>
2017-05-27 17:39:28 +0000

    adding winston logger with daily rotate file

--- config/log.js
+++ config/log.js
@@ -1,29 +1,33 @@
-/**
- * Built-in Log Configuration
- * (sails.config.log)
- *
- * Configure the log level for your app, as well as the transport
- * (Underneath the covers, Sails uses Winston for logging, which
- * allows for some pretty neat custom transports/adapters for log messages)
- *
- * For more information on the Sails logger, check out:
- * http://sailsjs.org/#!/documentation/concepts/Logging
- */
-
-module.exports.log = {
-
-  /***************************************************************************
-  *                                                                          *
-  * Valid `level` configs: i.e. the minimum log level to capture with        *
-  * sails.log.*()                                                            *
-  *                                                                          *
-  * The order of precedence for log levels from lowest to highest is:        *
-  * silly, verbose, info, debug, warn, error                                 *
-  *                                                                          *
-  * You may also set the level to "silent" to suppress all logs.             *
-  *                                                                          *
-  ***************************************************************************/
-
-  // level: 'info'
-
+var winston = require('winston');
+module.exports = {
+    'log': {
+          'colors': false,
+        'custom': new (winston.Logger)({
+            'transports': [
+                new (winston.transports.Console)({
+                    'level': 'info',
+                    'colorize': false,
+                    'timestamp': false,
+                    'json': false
+                }),
+                new winston.transports.File({
+                    'level': 'info   ',
+                    'colorize': false,
+                    'timestamp': true,
+                    'json': true,
+                    'filename': './logs/test.log',
+                    'maxsize': 5120000,
+                    'maxFiles': 3
+                }),
+                new winston.transports.DailyRotateFile({
+                    'filename': './logs/info-',
+                    'datePattern': 'yyyy-MM-dd.log',
+                    'prepend': false,
+                    'level': 'info',
+                    'colorize':false,
+                    
+                })
+            ]
+        })
+    }
 };
